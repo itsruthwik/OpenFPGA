63e7951a210e878ce331fe6e1d9614127dc1b435
Ruthwik <itsruthwik@outlook.com>
2024-05-30 02:51:27 -0700

    added missing ports

--- openfpga_flow/benchmarks/router/router_benchmark.v
+++ openfpga_flow/benchmarks/router/router_benchmark.v
@@ -1,41 +1,70 @@
 module router_benchmark(    
     input clk,  
     input reset,  
-    input [0:3] router_address,
-    input [0:67]channel_in_ip, 
-    input [0:1] flow_ctrl_in_op,
-    output error,
-    output [0:67] channel_out_op,
-    output [0:1] flow_ctrl_out_ip
+    input [0:3] ROUTER_ADDRESS,
+    input [0:67]CHANNEL_IN_IP, 
+    input [0:1] FLOW_CTRL_IN_OP,
+
+    output ERROR,
+
+    
+    output [0:67] CHANNEL_OUT_OP,
+
+
+    output [0:1] FLOW_CTRL_OUT_IP,
+
+
+    // output cout,
+    // output [3:0] sum, 	
+    // input cin,
+    // input [3:0] a,
+    // input [3:0] b  
+
 );
+
+    
+    wire [0:67] CHANNEL_ROUTER_1_OP;
+    wire [0:67] CHANNEL_ROUTER_2_OP;
+
+    wire [0:1] FLOW_CTRL_ROUTER_1_OP;
+    wire [0:1] FLOW_CTRL_ROUTER_2_OP;
+
+    wire ERROR1 ;
+    wire ERROR2 ;
+
+
 
 router router1      
     (.clk(clk),
     .reset(reset),
-    .router_address(router_address),
-    .channel_in_ip(channel_in_ip),
-    .flow_ctrl_in_op(flow_ctrl_in_op),
-    .channel_out_op(channel_router_1_op),
-    .flow_ctrl_out_ip(flow_ctrl_router_1_op),
-    .error(error1)
+    .ROUTER_ADDRESS(ROUTER_ADDRESS),
+    .CHANNEL_IN_IP(CHANNEL_IN_IP),
+    .FLOW_CTRL_IN_OP(FLOW_CTRL_IN_OP),
+    .CHANNEL_OUT_OP(CHANNEL_ROUTER_1_OP),
+    .FLOW_CTRL_OUT_IP(FLOW_CTRL_ROUTER_1_OP),
+    .ERROR(ERROR1)
     );
 
 router router2      
     (.clk(clk),
     .reset(reset),
-    .router_address(router_address + 1),
-    .channel_in_ip(channel_router_1_op),
-    .flow_ctrl_in_op(flow_ctrl_router_1_op),
-    .channel_out_op(channel_router_2_op),
-    .flow_ctrl_out_ip(flow_ctrl_router_2_op),
-    .error(error2)
+    .ROUTER_ADDRESS(ROUTER_ADDRESS + 1),
+    .CHANNEL_IN_IP(CHANNEL_ROUTER_1_OP),
+    .FLOW_CTRL_IN_OP(FLOW_CTRL_ROUTER_1_OP),
+    .CHANNEL_OUT_OP(CHANNEL_ROUTER_2_OP),
+    .FLOW_CTRL_OUT_IP(FLOW_CTRL_ROUTER_2_OP),
+    .ERROR(ERROR2)
     );
 
 
-assign flow_ctrl_out_ip = flow_ctrl_router_2_op ;
-assign channel_out_op = channel_router_2_op ;
-assign error = erro1 | erro2 ;
+assign FLOW_CTRL_OUT_IP = FLOW_CTRL_ROUTER_2_OP ;
+assign CHANNEL_OUT_OP = CHANNEL_ROUTER_2_OP ;
+assign ERROR = ERROR1 | ERROR2 ;
    
+
+// assign {cout, sum} = a + b + cin;
+
+
 endmodule
 
 /*
